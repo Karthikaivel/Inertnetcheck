64b42e27f931a35d809347bbc63ef38d965ad5e1
Karthikaivel95 <karthiksuthan234@gmail.com>
2018-09-05 12:22:27 +0530

    Check Internet Connection

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,34 @@
-import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {NavController, AlertController, Platform} from 'ionic-angular';
 
+declare var navigator: any;
+declare var Connection: any;
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
 })
 export class HomePage {
+    constructor(private navCtrl: NavController, private platform: Platform, private ac: AlertController) { }
 
-  constructor(public navCtrl: NavController) {
-
-  }
+    checkNetwork() {
+        this.platform.ready().then(() => {
+            var networkState = navigator.connection.type;
+            var states = {};
+            states[Connection.UNKNOWN]  = 'Unknown connection';
+            states[Connection.ETHERNET] = 'Ethernet connection';
+            states[Connection.WIFI]     = 'WiFi connection';
+            states[Connection.CELL_2G]  = 'Cell 2G connection';
+            states[Connection.CELL_3G]  = 'Cell 3G connection';
+            states[Connection.CELL_4G]  = 'Cell 4G connection';
+            states[Connection.CELL]     = 'Cell generic connection';
+            states[Connection.NONE]     = 'No network connection';
+        let alert = this.ac.create({
+                title: "Connection Status",
+                subTitle: states[networkState],
+                buttons: ["OK"]
+            });
+            alert.present();
+        });
+    }
 
 }
